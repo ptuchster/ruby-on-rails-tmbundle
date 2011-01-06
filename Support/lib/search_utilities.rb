be15365c5b5e56b28e5e4dad2f91f1abf9cf4df3
Philippe Huibonhoa <philippe.huibonhoa@bookrenter.com>
2011-01-05 17:24:12 -0800

    Updated searching capabilities to try the reverse order split off _ and ' '

--- Support/lib/search_utilities.rb
+++ Support/lib/search_utilities.rb
@@ -1,17 +1,30 @@
 require File.join(File.dirname(__FILE__), 'rails_bundle_tools')
 
 def array_sorted_search(array, search_term, &block)
+  out = single_array_sorted_search(array, search_term, &block)
+
+  if search_term =~ /[_ ]/
+    parts = search_term.split(/[_ ]/)
+    alternate_term = parts[-1] + ' ' + parts[0..-2].join(" ").strip
+    out += (single_array_sorted_search(array, alternate_term, &block))
+  end
+  
+  return out
+end
+
+def single_array_sorted_search(array, search_term, &block)
   block = lambda { |e| e } if block.nil?
   search_term = search_term.to_s
   matches = array.select { |name| block.call(name) =~ /.*#{search_term.gsub(/[^a-zA-Z0-9]/, '.*')}.*/i }
   sort_matches!(matches, search_term)
 
+  #searches for matches where letters all exist in the same order as search_term
   secondary_matches = array.select { |name| block.call(name) =~ /.*#{search_term.gsub(/[^a-zA-Z0-9]/, '').split(//).join('.*')}.*/i}
   sort_matches!(secondary_matches, search_term)
-
+  
   matches = matches + secondary_matches
   matches.uniq!
-  return matches
+  return matches  
 end
 
 def sort_matches!(matches, search_term)
